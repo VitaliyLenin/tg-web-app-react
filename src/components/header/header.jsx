77a794666404686569c08c45771245538004419e
VitaliyLenin <vitaliylenin77@gmail.com>
2023-07-23 13:58:03 +0300

    Update

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Button from "../button/button";
 import { useTelegram } from "../../hooks/useTelegram";
 
-const Header = (props) => {
+const Header = () => {
   const { user, onClose } = useTelegram();
   return (
     <div className={"header"}>
