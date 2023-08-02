e5fa2e3615035303acdbd9f26399920499c03553
VitaliyLenin <vitaliylenin77@gmail.com>
2023-08-02 23:14:48 +0300

    Add productList and productItems

---
+++ src/components/productItem/productItem.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Button from "./../button/button";
+
+import css from "./productItem.css";
+
+const ProductItem = ({ product, className, onAdd }) => {
+  const onAddHandler = () => {
+    onAdd(product);
+  };
+
+  return (
+    <div className={"product" + className}>
+      <div className={"img"} />
+      <div className={"title"}>{product.title}</div>
+      <div className={"description"}>{product.description}</div>
+      <div className={"price"}>
+        <span>
+          {" "}
+          Вартість: <b>{product.price}</b>
+        </span>
+      </div>
+      <Button className={"add-btn"} onClick={onAddHandler}>
+        Додати в корзину
+      </Button>
+    </div>
+  );
+};
+
+export default ProductItem;
